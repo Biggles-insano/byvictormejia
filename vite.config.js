5a6defefa0c419b30404929f765d8344e4314bbb
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2025-06-03 16:59:19 -0600

    fix: set base to root for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/byvictormejia/', // 👈 nombre exacto de tu repositorio en GitHub
   plugins: [react()],
+  base: '/',
   test: {
     environment: 'jsdom',
     globals: true
