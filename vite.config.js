19fa5bb395baac9e9f1184b45d6d3d7355b7d617
Samuel Mejía <150869146+Biggles-insano@users.noreply.github.com>
2025-06-03 16:31:50 -0600

    vercel

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/byvictormejia/', // 👈 nombre exacto de tu repositorio en GitHub
   plugins: [react()],
   test: {
     environment: 'jsdom',
